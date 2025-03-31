f70fa2a96ee1d6a94f4e4486309867d776699b82
Adri <miri.unza0192@gmail.com>
2025-03-30 22:08:02 -0400

    subiendo python

---
+++ ejercicio.py
@@ -0,0 +1,12 @@
+numero1 = int(input("Coloca tu primer numero: "))
+numero2 = int(input("Coloca tu segundo numero: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("El resultado de la suma es: " , suma)
+print("El resultado de la resta es: " , resta)
+print("El resultado de la multiplicación es: ", multiplicacion)
+print("El resultado de la división es: " , division)
